6410144d3b0768125edf79102ad6beb59cbe6aea
Shashi Khanal <dragonSlayer@Shashis-MacBook-Air.local>
2016-05-28 00:15:04 +0545

    minor fix

--- server.js
+++ server.js
@@ -2,10 +2,6 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 8000 ;
 
-var request = require('request');
-var cheerio = require('cheerio');
-var fs = require('fs');
-
 require('./routes.js')(app);
 
 app.listen(port, function() {
